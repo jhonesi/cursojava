7765eac72181ee0b7a95b3f0012603b4fdb99db7
jhonesi <jhoan2009dinas@gmail.com>
2025-10-14 18:11:54 -0500

    Add files via upload

---
+++ ejercicio_1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String username="abigail";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        //proceso
+        sumaDulces= cofreUno+cofreDos;
+        
+        //salida
+        System.out.print(username + "abigail ahora tiene un total de " + sumaDulces + " caramelos magicos");
+    }
+    
+}
---
+++ ejercicio_2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ejercicio_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        String aprendiz="jhoan";
+        
+        double primerReto=3.5;
+        double segundoReto=4.2;
+        double tercerReto=2.8;
+        double Promedio;
+                
+        //proceso
+        Promedio=primerReto+segundoReto+tercerReto/3;
+        
+        //salida
+        
+        System.out.print("tu promedio final es de " + Promedio + "has superado el nivel jeje");
+        
+        
+                
+    }
+    
+}
